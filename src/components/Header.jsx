5b8a608299ab07ee433bb6dbd92562904bbd4137
pedro.almeida <pedro.almeida@rtcom.pt>
2025-02-14 10:51:39 +0000

    Update Title Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logoImg from '../assets/quiz-logo.png';
 export default function Header(){
     return (
         <header>
-            <img src={logoImg} alt="Quiz Logo"/>
+            <img src={logoImg} alt="Quizz Logo"/>
             <h1>React Quizz</h1>
         </header>
     );
